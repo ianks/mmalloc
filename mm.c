23f8dd292da8a79e1935cb823cd44cf737729e98
Ian Ker-Seymer <i.kerseymer@gmail.com>
2014-04-26 15:03:24 -0600

    Implement find fit and place

--- mm.c
+++ mm.c
@@ -195,7 +195,16 @@ static void *extend_heap(size_t words)
 //
 static void *find_fit(size_t asize)
 {
-  return NULL; /* no fit */
+  // first fit search
+  void *bp;
+
+  for (bp = heap_listp; GET_SIZE(HDRP(bp)) > 0; bp = NEXT_BLKP(bp)){
+    if ( !GET_ALLOC(HDRP(bp)) && (asize <= GET_SIZE(HDRP(bp)))) {
+        return bp;
+    }
+  }
+  //no fit
+  return NULL;
 }
 
 //
@@ -305,8 +314,21 @@ void *mm_malloc(size_t size)
 //
 static void place(void *bp, size_t asize)
 {
-}
+  size_t csize = GET_SIZE(HDRP(bp));
 
+  if ((csize - asize) >= (2*DSIZE)){
+    PUT(HDRP(bp), PACK(asize, 1));
+    PUT(FTRP(bp), PACK(asize, 1));
+    bp = NEXT_BLKP(bp);
+    PUT(HDRP(bp), PACK(csize - asize, 0));
+    PUT(FTRP(bp), PACK(csize - asize, 0));
+  }
+
+  else{
+    PUT(HDRP(bp), PACK(csize,1));
+    PUT(FTRP(bp), PACK(csize,1));
+  }
+}
 
 //
 // mm_realloc -- implemented for you
